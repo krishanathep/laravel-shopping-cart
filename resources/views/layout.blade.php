891f770d7832d0af9af9fdb6b84fdbefdd24bbf9
krishanathep <krishanathep@gmail.com>
2021-09-22 09:52:09 +0700

    first deploy

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Add To Cart Function - ItSolutionStuff.com</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- Sweet Alert -->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">Shopping</a>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/') }}">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('cart') }}">
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart <span class="badge badge-pill badge-danger">{{ count((array) session('cart')) }}</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <br />
+    <div class="container">
+
+        @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+        @endif
+
+        @yield('content')
+    </div>
+
+    @yield('scripts')
+
+</body>
+</html>
---
+++ resources/views/products.blade.php
@@ -0,0 +1,21 @@
+@extends('layout')
+
+@section('content')
+
+<div class="row">
+    @foreach($products as $product)
+    <div class="col-xs-18 col-sm-6 col-md-3">
+        <div class="card mt-2 mb-2">
+            <img class="card-img-top" src="{{ $product->image }}" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">{{ $product->name }}</h4>
+              <p class="card-text">{{ $product->description }}</p>
+              <p class="card-text"><strong>Price: </strong> {{ $product->price }}$</p>
+              <a href="{{ route('add.to.cart', $product->id) }}" class="btn btn-info btn-block">Add to cart</a>
+            </div>
+        </div>
+    </div>
+    @endforeach
+</div>
+
+@endsection
